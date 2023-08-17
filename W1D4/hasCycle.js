7d2db27a806885f0d396287979d38e969eb3a26a
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-17 11:53:42 -0700

    Completed hasCycle

--- W1D4/hasCycle.js
+++ W1D4/hasCycle.js
@@ -1,1 +1,14 @@
 // https://leetcode.com/problems/linked-list-cycle/
+
+const hasCycle = (head) => {
+
+  if (!head || !head.next) { return false; }
+
+  head.val = null;
+
+  if (head.next.val === null) {
+    return true;
+  } else {
+    return hasCycle(head.next);
+  }
+};
