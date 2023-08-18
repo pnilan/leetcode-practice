0060d88fd3d46cb27ee61a5dceae5a7c7568d296
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-18 14:14:54 -0700

    completed isBalanced

--- W1D5/isBalanced.js
+++ W1D5/isBalanced.js
@@ -31,8 +31,21 @@ The number of nodes in the tree is in the range [0, 5000].
 // recursive
 // Compare depth of left and right children
 
-const isBalanced = (root) => {
+var isBalanced = function(root) {
+  if (!root) { return true; }
+  if (maxDepth(root) === -1) { return false; }
+  return true;
+};
 
+var maxDepth = (root) => {
 
+  if (!root) { return 0; }
 
+  let leftDepth = maxDepth(root.left);
+  let rightDepth = maxDepth(root.right);
+
+  if (leftDepth === -1 || rightDepth === -1) { return -1; }
+  if (Math.abs(leftDepth - rightDepth) > 1) { return -1; }
+
+  return Math.max(leftDepth, rightDepth) + 1;
 };
