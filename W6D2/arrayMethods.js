789b725aa5f53d86558efcab7709dcb72c10b169
Patrick Nilan <nilan.patrick@gmail.com>
2023-09-19 12:04:35 -0700

    Created array methods cheat sheet

---
+++ W6D2/arrayMethods.js
@@ -0,0 +1,15 @@
+/**
+ * Array methods review cheat sheet
+ * .length => return length of array
+ * .toString() => converts array to comma separates strings
+ * .pop() => removes the last element of an array and returns that element
+ * .push(el) => adds "el" to the end of an array
+ * .shift() => removes the first element of an array and returns that element
+ * .unshift(el) => adds "el" to the beginning of an array
+ * .join() => combines elements of an array into a string separated by string argument passed to it
+ * .delete() => deletes an element from an array but keeps array length intact and replaces element with undefined
+ * .concat(array) => Combines two arrays by merging into a single array
+ * .flat() => Flattens array of subarrays into a flat array
+ * .splice() => adds new element(s) to array in location identified by arguments
+ * .slice(i, j) => returns a copy of the array from index i to j (exclusive)
+ */
