83a0f4cb9e9577a4d62f66e64511df8181a3ae7a
Patrick Nilan <nilan.patrick@gmail.com>
2023-11-27 14:10:29 -0800

    Completed largestRectangleArea again

--- largestRectangle/largestRectangle2.js
+++ largestRectangle/largestRectangle2.js
@@ -8,6 +8,8 @@
  * Edge Cases: height of zero
  *
  * Strategy:
+ * A rectangle can only extend right if the rectangle to its right is same height or higher, therefore, if a "rectangle" is shorter than the previous rectangle, you can calculate the area of the previous rectangle and pop from stack
+ *
  *
  * @param {number[]} heights
  * @return {number}
@@ -15,22 +17,22 @@
 
 const largestRectangleArea = (heights) => {
 
-  let stack = [];
   let maxArea = 0;
+  let stack = [];
 
   for (let i = 0; i < heights.length; i++) {
     let start = i;
-    while (stack.length && stack[stack.length - 1][1] > heights[i]) {
-      let [stackIdx, stackHeight] = stack.pop();
-      let currentHeight = stackHeight * (i - stackIdx);
-      maxArea = Math.max(maxArea, currentHeight);
-      start = stackIdx;
+    while (stack.length && heights[i] < stack[stack.length - 1][1]) {
+      let prevRect = stack.pop();
+      maxArea = Math.max(maxArea, (i - prevRect[0]) * prevRect[1]);
+      start = prevRect[0];
     }
+
     stack.push([start, heights[i]]);
   }
 
-  for (let height of stack) {
-    maxArea = Math.max(maxArea, height[1] * (heights.length - height[0]));
+  for (let i = stack.length - 1; i >= 0; i--) {
+    maxArea = Math.max(maxArea, (heights.length - stack[i][0]) * stack[i][1]);
   }
 
   return maxArea;
@@ -50,3 +52,4 @@ heights = [2, 1, 2];
 expected = 3;
 actual = largestRectangleArea(heights);
 console.log(`Expected ${expected}, actual is ${actual}. Correct answer? ${expected === actual}`);
+
