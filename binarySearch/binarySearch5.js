6a7308884d753eaa6de203f44df684ed18c92e0d
Patrick Nilan <nilan.patrick@gmail.com>
2023-12-08 13:29:18 -0800

    Completed binary search

---
+++ binarySearch/binarySearch5.js
@@ -0,0 +1,20 @@
+const search = (nums, target) => {
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = (left + right) >> 1;
+
+    if (target < nums[mid]) {
+      right = mid - 1;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+
+};
